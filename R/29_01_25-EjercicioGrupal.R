fc5a8731633e070cf6272118c407ce15d698b178
JordiGaGa <jordigg@lcg.unam.mx>
2025-01-29 17:10:31 -0600

    Ejercicio Grupal, escrito

---
+++ R/29_01_25-EjercicioGrupal.R
@@ -0,0 +1,36 @@
+Autores:
+  Katharina Waury
+
+Nombre del paquete:
+  immunogenViewer
+
+Descripción del paquete:
+  Plots protein properties and visualizes position of peptide immunogens within
+  protein sequence. Allows evaluation of immunogens based on structural and
+  functional annotations to infer suitability for antibody-based methods aiming
+  to detect native proteins.
+
+DOI:
+  10.18129/B9.bioc.immunogenViewer
+
+Plataformas:
+  Todas
+
+Soporte:
+  0
+
+Dependencias:
+  87
+
+Git Hub:
+  https://github.com/kathiwaury/immunogenViewer
+
+
+Discusión:
+  Existen muchos paquetes muy diversos en Bioconductor, los cuales varian en su
+  nivel de completitud y de entendimiento para el usuario, así como muchos otros
+  aspectos como en qué sistemas operativos funciona, soporte, dependencias, etc.
+  Considero que es bastante bueno y adecuado que tengan muchos paquetes de
+  bioinformática con una organización que permita al usuario interactuar y
+  aprovecharlos de mejor manera.
+
